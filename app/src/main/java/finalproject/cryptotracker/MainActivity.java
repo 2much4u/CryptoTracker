d3d81fb8073f12cfe9ff080ddf66d0847db53d3d
2much4u <christoffwk@gmail.com>
2018-12-09 22:24:38 -0600

    Fixed status bar overlapping app

--- app/src/main/java/finalproject/cryptotracker/MainActivity.java
+++ app/src/main/java/finalproject/cryptotracker/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import java.util.List;
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         RecyclerView recyclerView = findViewById(R.id.coinRecycler);
         List<CoinItem> coins = new ArrayList<>();
